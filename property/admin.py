1d625d4374a06224840e39f54e593d076c9c9c59
owwwl666 <zaharsalam228@gmail.com>
2023-07-17 20:57:21 +1000

    removed fields from model Flat

--- property/admin.py
+++ property/admin.py
@@ -7,7 +7,6 @@ from .models import Owner
 
 class FlatAdmin(admin.ModelAdmin):
     search_fields = [
-        "owner",
         "town",
         "address",
     ]
@@ -18,8 +17,6 @@ class FlatAdmin(admin.ModelAdmin):
         "new_building",
         "construction_year",
         "town",
-        "owners_phonenumber",
-        "owner_pure_phone",
     ]
     list_editable = ["new_building"]
     list_filter = [
--- property/models.py
+++ property/models.py
@@ -5,11 +5,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
-    owner_pure_phone = PhoneNumberField(region="RU", blank=True, verbose_name="Нормализованный номер владельца")
     new_building = models.BooleanField(blank=True, null=True, verbose_name="Новостройка")
-
     description = models.TextField('Текст объявления', blank=True)
     price = models.IntegerField('Цена квартиры', db_index=True)
 
